a316178822ca695e49df240bdde7862008fc18f7
Sergei Shcherbak <sshch1987@gmail.com>
2022-12-12 17:14:53 +0200

    Comments and readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# youload
-Very simple YouTube playlist downloader (using pytube library)
+# YouLoad #
+
+Very simple YouTube playlist downloader (using pytube library).
+(Perhaps this will become a more useful GUI application later.)
+
+
+Please note the list item format: that should look like [Playlist(your_link), Playlist(your_link_1)]
+
+I didn't aim to put here all the existing formats that are possible to get from YouTube, you can experiment on your own. 
--- YouLoad.py
+++ YouLoad.py
@@ -1,15 +1,21 @@
 from pytube import Playlist
 
-playlists = [Playlist("https://www.youtube.com/playlist?list=whatever/put_here_any_list_you_want")]
+# Setting up the playlist/list of playlists you might want to download
+playlists = [Playlist("https://www.youtube.com/playlist?list=whatever/put_here_any_playlist_link_you_want")]
+# Setting up the list of target video formats
 required_ext = ['mp4']
 
 
 def load_me_plently(target_pl, ext):
-    print(f'Downloading: {target_pl.title}')
+    """ Loading all the videos from the target YouTube playlist """
+    print(f'Downloading: {target_pl.title}')  # Writing a playlist name in console
+    # iterating all the videos on the playlist:
     for video in target_pl.videos:
-        print(f'Downloading: {video.title}')
+        print(f'Downloading: {video.title}')  # See what video is currently downloading
+        # downloading:
         video.streams.filter(file_extension=f'{ext}').first().download(f"./{target_pl.title}")
 
 
 if __name__ == '__main__':
+    # calling the function/ target playlist and required video format are the arguments
     load_me_plently(playlists[0], required_ext[0])
