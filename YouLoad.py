de8ef8a7347b8c61d33373cad50573ae9052a52d
Sergei Shcherbak <sshch1987@gmail.com>
2022-12-12 16:46:38 +0200

    Update YouLoad.py

--- YouLoad.py
+++ YouLoad.py
@@ -1,6 +1,6 @@
 from pytube import Playlist
 
-playlists = [Playlist("https://www.youtube.com/playlist?list=PLPrskxx43-yngz6Cz7x_tWas4hpzsrE3P")]
+playlists = [Playlist("https://www.youtube.com/playlist?list=whatever/put_here_any_list_you_want")]
 required_ext = ['mp4']
 
 
@@ -8,8 +8,7 @@ def load_me_plently(target_pl, ext):
     print(f'Downloading: {target_pl.title}')
     for video in target_pl.videos:
         print(f'Downloading: {video.title}')
-        vids = video.streams.filter(file_extension=f'{ext}')
-        vids.first().download(f"./{target_pl.title}")
+        video.streams.filter(file_extension=f'{ext}').first().download(f"./{target_pl.title}")
 
 
 if __name__ == '__main__':
