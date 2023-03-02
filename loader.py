32c98ed9d4e4c09324be73fa306a2737106a147d
Sergei Shcherbak <sshch1987@gmail.com>
2023-03-02 21:34:27 +0200

    Create loader.py

---
+++ loader.py
@@ -0,0 +1,14 @@
+from pytube import Playlist
+import random
+import time
+import pytube.request
+from pytube import YouTube
+from pytube.cli import on_progress
+
+
+class Clip:
+    ...
+
+
+class Videolist:
+    ...
