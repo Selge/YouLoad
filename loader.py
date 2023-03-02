65da9131607337b5712e5811b7240014bc2246c3
Sergei Shcherbak <sshch1987@gmail.com>
2023-03-02 21:47:21 +0200

    Update loader.py

--- loader.py
+++ loader.py
@@ -10,8 +10,20 @@ single_url = str(input("Please enter the single video URL:  ")).lower()
 playlists = [Playlist(str(input("Please enter the playlist URL:  "))).lower()]
 required_ext = ['mp4']
 pytube.request.default_range_size = 500_000
-yt = YouTube(single_url, on_progress_callback=on_progress, on_complete_callback=on_complete)
+yt = YouTube(single_url)
 stream = yt.streams.filter(progressive=True, file_extension=required_ext[0]).get_highest_resolution()
+
+
+def on_complete(a, b):
+    completed = "\nDownload completed!\n"
+    size = 'File Size: ' + str(stream.filesize/1_000_000) + 'Mb\n'
+    title = 'Title: ' + stream.title + '\n'
+    desc = 'Description: ' + yt.description + '\n'
+    author = 'Author: ' + yt.author + '\n'
+    length = 'Video length: ' + str(yt.length) + 'Seconds\n'
+
+    txt_list = f'{completed}, {title}, {author}, {desc}, {length}, {size}'
+    return txt_list
 
 
 class Clip:
