73c3c25e86359ea66e4c5da675268b2c864f10e9
Sergei Shcherbak <sshch1987@gmail.com>
2023-03-02 21:27:59 +0200

    Update window.py

--- window.py
+++ window.py
@@ -6,3 +6,11 @@ def main(page: flet.Page):
     page.vertical_alignment = "center"
     page.window_height = 800
     page.window_width = 600
+
+
+def window():
+    flet.app(target=main)
+
+
+if __name__ == "__main__":
+    window()
