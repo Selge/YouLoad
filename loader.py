5014d219709d3f1a51611d7945a2de2790406c11
Sergei Shcherbak <sshch1987@gmail.com>
2023-03-02 22:03:02 +0200

    Update loader.py

--- loader.py
+++ loader.py
@@ -6,17 +6,16 @@ from pytube import YouTube
 from pytube.cli import on_progress
 
 
-single_url = str(input("Please enter the single video URL:  ")).lower()
 playlists = [Playlist(str(input("Please enter the playlist URL:  "))).lower()]
 required_ext = ['mp4']
 pytube.request.default_range_size = 500_000
-yt = YouTube(single_url)
-stream = yt.streams.filter(progressive=True, file_extension=required_ext[0]).get_highest_resolution()
 
 
 class Clip:
     def __init__(self):
-        self.video = single_url
+        self.video = str(input("Please enter the single video URL:  ")).lower()
+        self.yt = YouTube(self.video)
+        self.stream = self.yt.streams.filter(progressive=True, file_extension=required_ext[0]).get_highest_resolution()
 
     def load_me_single(self, video):
         video.stream.download(f"./{video.title}")
