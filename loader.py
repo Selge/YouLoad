666f895b515e8907b82fe5ebf3c2c160527ef127
Sergei Shcherbak <sshch1987@gmail.com>
2023-03-03 11:21:55 +0200

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,9 +1,6 @@
-from pytube import Playlist
-import random
-import time
 import pytube.request
 from pytube import YouTube
-from pytube.cli import on_progress
+from pytube import Playlist
 
 
 playlist = [Playlist(str(input("Please enter the playlist URL:  "))).lower()]
