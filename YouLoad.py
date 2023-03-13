d2dd10dff3e0634ee3cb711d3289341a905f0466
Sergei Shcherbak <sshch1987@gmail.com>
2023-03-13 22:42:01 +0300

    Update YouLoad.py

--- YouLoad.py
+++ YouLoad.py
@@ -11,6 +11,7 @@ from pytube.cli import on_progress
 4
 5
 6
+7
 
 # Setting up the playlist/list of playlists you might want to download
 single_url = str(input("Please enter the single video URL:  ")).lower()
